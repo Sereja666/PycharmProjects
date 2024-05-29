61545c45ed130579e35ee1dbf326362c77671e2b
Sereja <60959530+Sereja666@users.noreply.github.com>
2024-05-29 11:08:38 +0300

    Update config.py

--- clinicist_robot/config.py
+++ clinicist_robot/config.py
@@ -1,19 +1,1 @@
-# -*- coding: utf-8 -*-
-#сайт
-login_site = 'info@clinicist.ru'
-password_site = 'n3daGEfdSDF'
 
-#ftp
-host = 'ftp.aaz.1gb.ru'
-ftp_user = 'w_aaz_8862b62e'
-ftp_password = 'aza772d5d456'
-
-#время запуска обзвона
-start_call = '21:05'
-#время сохранения с фтп
-start_download = '21:15'
-
-# где сохранить файлс с фтп
-# ftp_file = '../MyData.csv'
-new_place = r'C:\Users\Sereja\Documents\Vova'
-path = r'C:\Users\Sereja\Downloads'
